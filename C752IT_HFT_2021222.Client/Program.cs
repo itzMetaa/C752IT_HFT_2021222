13adc6b0f5374f9e6f1880e7000a600c35827547
itzMetaa <bajkaipatrik4@gmail.com>
2022-05-10 21:11:28 +0200

    fixed dependencies

--- C752IT_HFT_2021222.Client/Program.cs
+++ C752IT_HFT_2021222.Client/Program.cs
@@ -9,23 +9,7 @@ namespace C752IT_HFT_2021222.Client
     {
         static void Main(string[] args)
         {
-            IRepository<Game> repo = new GameRepository(new GameDbContext());
-            //var items = repo.ReadAll().ToArray();
 
-            var g = new Game()
-            {
-                Title = "tesztGame",
-                Description = "Ez egy teszt elem",
-                Type = GameType.Indie,
-                Price = 40,
-            };
-            repo.Create(g);
-
-            var another = repo.Read(1);
-            another.Title = "teszt2";
-            repo.Update(another);
-
-            var items = repo.ReadAll().ToArray();
             ;
         }
     }
