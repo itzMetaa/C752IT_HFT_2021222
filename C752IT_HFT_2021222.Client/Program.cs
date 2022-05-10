37f87616283bb9637396acc70e884c8c1867dea7
itzMetaa <bajkaipatrik4@gmail.com>
2022-05-11 01:28:00 +0200

    Made the basic menu system and also implemented a working listing to the user interface

--- C752IT_HFT_2021222.Client/Program.cs
+++ C752IT_HFT_2021222.Client/Program.cs
@@ -3,11 +3,63 @@ using C752IT_HFT_2021222.Repository;
 using C752IT_HFT_2021222.Logic;
 using System;
 using System.Linq;
+using ConsoleTools;
 
 namespace C752IT_HFT_2021222.Client
 {
     class Program
     {
+        static GameLogic gameLogic;
+        static DeveloperLogic developerLogic;
+        static PublisherLogic publisherLogic;
+        static void Create(string entity)
+        {
+            Console.WriteLine(entity + " create");
+            Console.ReadLine();
+        }
+        static void List(string entity)
+        {
+            switch (entity)
+            {
+                case "Game":
+                    var items = gameLogic.ReadAll();
+                    Console.WriteLine("Id" + "\t" + "Title");
+                    foreach (var item in items)
+                    {
+                        Console.WriteLine($"{item.Id}\t{item.Title}");
+                    }
+                    break;
+                case "Publisher":
+                    var items2 = publisherLogic.ReadAll();
+                    Console.WriteLine("Id" + "\t" + "Name");
+                    foreach (var item in items2)
+                    {
+                        Console.WriteLine($"{item.Id}\t{item.Name}");
+                    }
+                    break;
+                case "Developer":
+                    var items3 = developerLogic.ReadAll();
+                    Console.WriteLine("Id" + "\t" + "Name");
+                    foreach (var item in items3)
+                    {
+                        Console.WriteLine($"{item.Id}\t{item.Name}");
+                    }
+                    break;
+            }
+            Console.WriteLine(entity + " list");
+            Console.ReadLine();
+        }
+        static void Update(string entity)
+        {
+            Console.WriteLine(entity + " update");
+            Console.ReadLine();
+        }
+        static void Delete(string entity)
+        {
+            Console.WriteLine(entity + " delete");
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             var ctx = new GameDbContext();
@@ -15,15 +67,41 @@ namespace C752IT_HFT_2021222.Client
             var rd = new DeveloperRepository(ctx);
             var rp = new PublisherRepository(ctx);
 
-            var lg = new GameLogic(rg);
-            var ld = new DeveloperLogic(rd);
-            var lp = new PublisherLogic(rp);
+            gameLogic = new GameLogic(rg);
+            developerLogic = new DeveloperLogic(rd);
+            publisherLogic = new PublisherLogic(rp);
 
-            var item2 = lg.GetGameRevenueInfo();
-            var i = item2.Max(i => i.TotalRevenue);
-            var item3 = lg.GetMostProfitableGame();
+            var publisherSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Publisher"))
+                .Add("Create", () => Create("Publisher"))
+                .Add("Delete", () => Delete("Publisher"))
+                .Add("Update", () => Update("Publisher"))
+                .Add("Exit", ConsoleMenu.Close)
+                ;
+            var developerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Developer"))
+                .Add("Create", () => Create("Developer"))
+                .Add("Delete", () => Delete("Developer"))
+                .Add("Update", () => Update("Developer"))
+                .Add("Exit", ConsoleMenu.Close)
+                ;
 
-            ;
+            var gameSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", ()=>List("Game"))
+                .Add("Create", ()=>Create("Game"))
+                .Add("Delete", ()=>Delete("Game"))
+                .Add("Update", ()=>Update("Game"))
+                .Add("Exit", ConsoleMenu.Close)
+                ;
+
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Games", () => gameSubMenu.Show())
+                .Add("Developers", () => publisherSubMenu.Show())
+                .Add("Publishers", () => developerSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close)
+                ;
+
+            menu.Show();
         }
     }
 }
