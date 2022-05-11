7201ec83b7c13f1cc8827ed104bfe673af2313f6
itzMetaa <bajkaipatrik4@gmail.com>
2022-05-11 16:05:31 +0200

    at this point the app is ready and working with the game table, now I just have to fill up the crud and non crud implementations of the other tables

---
+++ C752IT_HFT_2021222.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,42 @@
+﻿using C752IT_HFT_2021222.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace C752IT_HFT_2021222.Endpoint.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        IGameLogic logic;
+
+        public StatController(IGameLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET api/<StatController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        public GameInfo MostProfitableGame()
+        {
+            return this.logic.GetMostProfitableGame();
+        }
+        [HttpGet]
+        public IEnumerable<GameInfo> GameRevenueInfo()
+        {
+            return this.logic.GetGameRevenueInfo();
+        }
+
+    }
+}
