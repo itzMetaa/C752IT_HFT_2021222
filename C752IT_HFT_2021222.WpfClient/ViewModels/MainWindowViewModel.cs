7dac9ad06f3515ac587089fe76dad2ee5c505bd1
itzMetaa <bajkaipatrik4@gmail.com>
2023-05-28 18:43:43 +0200

    basic viewmodel done. it can list games :)

---
+++ C752IT_HFT_2021222.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using C752IT_HFT_2021222.Models;
+using C752IT_HFT_2021222.RestClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C752IT_HFT_2021222.WpfClient.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Game> Games { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Games = new RestCollection<Game>("http://localhost:54503/", "game");
+        }
+    }
+}
