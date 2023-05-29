d88c703ef235c1a1a1599085d29ef1b53a8e1759
itzMetaa <bajkaipatrik4@gmail.com>
2023-05-29 22:05:02 +0200

    WPFClient finished Fully working non crud window added

--- C752IT_HFT_2021222.Endpoint/Controllers/StatController.cs
+++ C752IT_HFT_2021222.Endpoint/Controllers/StatController.cs
@@ -1,6 +1,9 @@
-﻿using C752IT_HFT_2021222.Logic;
+﻿using C752IT_HFT_2021222.Endpoint.Services;
+using C752IT_HFT_2021222.Logic;
 using C752IT_HFT_2021222.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,45 +20,53 @@ namespace C752IT_HFT_2021222.Endpoint.Controllers
         IGameLogic gameLogic;
         IDeveloperLogic developerLogic;
         IPublisherLogic publisherLogic;
+        IHubContext<SignalRHub> hub;
 
-        public StatController(IGameLogic logic, IDeveloperLogic devLogic, IPublisherLogic pubLogic)
+        public StatController(IGameLogic logic, IDeveloperLogic devLogic, IPublisherLogic pubLogic, IHubContext<SignalRHub> hub)
         {
             this.gameLogic = logic;
             this.developerLogic = devLogic;
             this.publisherLogic = pubLogic;
+            this.hub = hub;
         }
 
         //game logic
         [HttpGet]
         public GameInfo MostProfitableGame()
         {
+            this.hub.Clients.All.SendAsync("Stat1");
             return this.gameLogic.GetMostProfitableGame();
         }
         [HttpGet]
         public IEnumerable<GameInfo> GameRevenueInfo()
         {
+            this.hub.Clients.All.SendAsync("Stat2");
             return this.gameLogic.GetGameRevenueInfo();
         }
         [HttpGet]
         public double? AveragePriceOfGames()
         {
+            this.hub.Clients.All.SendAsync("Stat3");
             return this.gameLogic.GetAveragePriceOfGames();
         }
         [HttpGet]
         public IEnumerable<KeyValuePair<GameType, int>> NumberOfGamesPerType()
         {
+            this.hub.Clients.All.SendAsync("Stat4");
             return this.gameLogic.GetNumberOfGamesPerType();
         }
         //Publisher logic
         [HttpGet("{id}")]
         public IEnumerable<Game> GetGamesOfPublisher(int id)
         {
+            this.hub.Clients.All.SendAsync("GetGamesOfPublisher");
             return this.publisherLogic.GamesOfPublisher(id);
         }
         //Developer logic
         [HttpGet("{id}")]
         public IEnumerable<Game> GetGamesOfDevelopers(int id)
         {
+            this.hub.Clients.All.SendAsync("GetGamesOfDevelopers");
             return this.developerLogic.GamesOfDeveloper(id);
         }
     }
--- C752IT_HFT_2021222.WpfClient/ViewModels/StatisticsWindowViewModel.cs
+++ C752IT_HFT_2021222.WpfClient/ViewModels/StatisticsWindowViewModel.cs
@@ -1,12 +1,106 @@
-﻿using System;
+﻿using C752IT_HFT_2021222.Models;
+using C752IT_HFT_2021222.RestClient;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace C752IT_HFT_2021222.WpfClient.ViewModels
 {
-    internal class StatisticsWindowViewModel
+    internal class StatisticsWindowViewModel : ObservableRecipient
     {
+        public GameInfo MostProfitableGame { get; }
+        public double AveragePriceOfGames { get; }
+        public List<GameInfo> GameRevenueInfo { get; }
+        public List<KeyValuePair<GameType, int>> NumberOfGamesPerType { get; }
+        private List<Game> gamesOfPublisher;
+        public List<Game> GamesOfPublisher { get { return gamesOfPublisher; } 
+            set {
+                SetProperty(ref gamesOfPublisher, value); 
+                OnPropertyChanged(); 
+            } 
+        }
+        private List<Game> gamesOfDeveloper;
+        public List<Game> GamesOfDeveloper
+        {
+            get { return gamesOfDeveloper; }
+            set
+            {
+                SetProperty(ref gamesOfDeveloper, value);
+                OnPropertyChanged();
+            }
+        }
+        public ICommand PublisherListCommand { get; }
+        public ICommand DeveloperListCommand { get; }
+
+        public RestCollection<Publisher> Publishers { get; set; }
+        public RestCollection<Developer> Developers { get; set; }
+
+        public StatisticsWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Developers = new RestCollection<Developer>("http://localhost:54503/", "developer", "hub");
+                Publishers = new RestCollection<Publisher>("http://localhost:54503/", "publisher", "hub");
+
+                MostProfitableGame = new RestService("http://localhost:54503/stat/", "MostProfitableGame").GetSingle<GameInfo>("MostProfitableGame");
+                AveragePriceOfGames = new RestService("http://localhost:54503/stat/", "AveragePriceOfGames").GetSingle<double>("AveragePriceOfGames");
+                GameRevenueInfo = new RestService("http://localhost:54503/stat/", "GameRevenueInfo").Get<GameInfo>("GameRevenueInfo");
+                NumberOfGamesPerType = new RestService("http://localhost:54503/stat/", "NumberOfGamesPerType").Get<KeyValuePair<GameType, int>>("NumberOfGamesPerType");
+
+                PublisherListCommand = new RelayCommand
+                   (() =>
+                   {
+                       GamesOfPublisher = new RestService("http://localhost:54503/").Get<List<Game>>(SelectedPublisher.Id, "stat/GetGamesOfPublisher");
+                   }, ()=> SelectedPublisher != null
+                );
+                DeveloperListCommand = new RelayCommand
+                   (() =>
+                   {
+                       GamesOfDeveloper = new RestService("http://localhost:54503/").Get<List<Game>>(SelectedDeveloper.Id, "stat/GetGamesOfDevelopers");
+                   }, () => SelectedDeveloper != null
+                );
+
+                GamesOfPublisher = new List<Game>();
+                GamesOfDeveloper = new List<Game>();
+                SelectedPublisher = new Publisher();
+                SelectedDeveloper = new Developer();
+            }
+        }
+
+        private Developer selectedDeveloper;
+        public Developer SelectedDeveloper
+        {
+            get { return selectedDeveloper; }
+            set
+            {
+                SetProperty(ref selectedDeveloper, value);
+            }
+        }
+
+        private Publisher selectedPublisher;
+        public Publisher SelectedPublisher
+        {
+            get { return selectedPublisher; }
+            set
+            {
+                SetProperty(ref selectedPublisher, value);
+            }
+        }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
     }
 }
