305a8049d5e57a964bc633c1c459ab59e7c7f1a2
itzMetaa <bajkaipatrik4@gmail.com>
2023-05-31 18:14:44 +0200

    developers and publisher cruds working added 2 non cruds too

--- C752IT_HFT_2021222.JSClient/wwwroot/script.js
+++ C752IT_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,6 +1,17 @@
 ﻿let games = [];
+let developers = [];
+let publishers = [];
 let connection = null;
+let mostprofitablegame = [];
+let gamerevenueinfo = [];
+let averagepriceofgames = -1;
+let numberofgamespertype = [];
+let gamesofpublisher = [];
+let gamesofdeveloper = [];
+
 let gameidtoupdate = -1;
+let devidtoupdate = -1;
+let publisheridtoupdate = -1;
 
 
 getdata();
@@ -24,10 +35,35 @@ function setupSignalR() {
         getdata();
     });
 
+    connection.on("DeveloperCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("DeveloperUpdated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("DeveloperDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.on("PublisherCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("PublisherUpdated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("PublisherDeleted", (user, message) => {
+        getdata();
+    });
+
     connection.onclose(async () => {
         await start();
     });
     start();
+
 }
 
 async function start() {
@@ -46,25 +82,99 @@ async function getdata() {
     .then(x => x.json())
     .then(y => {
         games = y;
-        //console.log(games);
         display();
     });
 
+    await fetch('http://localhost:54503/developer')
+    .then(x => x.json())
+    .then(y => {
+        developers = y;
+        display();
+    });
+
+    await fetch('http://localhost:54503/publisher')
+    .then(x => x.json())
+    .then(y => {
+        publishers = y;
+        display();
+    });
+
+    await fetch('http://localhost:54503/stat/mostprofitablegame')
+        .then(x => x.json())
+        .then(y => {
+            mostprofitablegame = y;
+            display();
+        });
+
+    await fetch('http://localhost:54503/stat/gamerevenueinfo')
+        .then(x => x.json())
+        .then(y => {
+            gamerevenueinfo = y;
+            display();
+        });
+    await fetch('http://localhost:54503/stat/averagepriceofgames')
+        .then(x => x.json())
+        .then(y => {
+            averagepriceofgames = y;
+            display();
+        });
+    await fetch('http://localhost:54503/stat/numberofgamespertype')
+        .then(x => x.json())
+        .then(y => {
+            numberofgamespertype = y;
+            display();
+        });
 }
 
 function display() {
+    document.getElementById('averagepriceofgameslabel').innerHTML = "";
+    document.getElementById('averagepriceofgameslabel').innerHTML += "" + averagepriceofgames;
+
+    document.getElementById('mostprofitablegamerevenue').innerHTML = "";
+    document.getElementById('mostprofitablegamerevenue').innerHTML += "" + mostprofitablegame.totalRevenue; 
+
+
+    //console.log('******GAMES******')
     document.getElementById('resultarea').innerHTML = "";
     games.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
-            "<tr><td>" + t.id + "</td><td>" + t.title + "</td><td>" + t.price + "</td><td>" + t.rating + "</td><td>" + t.copiesSold + "</td><td>" + t.type + "</td><td>" + t.description + "</td><td>" + `<button type="button" onclick="remove(${t.id})">Delete</button>` + `<button type="button" onclick="showupdate(${t.id})">Update</button>` +"</td></tr>"
-        console.log(t.title);
-    })
+            "<tr><td>" + t.id + "</td><td>" + t.title + "</td><td>" + t.price + "</td><td>" + t.rating + "</td><td>" + t.copiesSold + "</td><td>" + t.type + "</td><td>" + t.description + "</td><td>" + `<button type="button" onclick="remove(${t.id})">Delete</button>` + `<button type="button" onclick="showupdate(${t.id})">Update</button>` + "</td></tr>"
+        //console.log(t.title);
+    });
+    //console.log('******DEVELOPERS******')
+    document.getElementById('resultareadevs').innerHTML = "";
+    developers.forEach(t => {
+        document.getElementById('resultareadevs').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>" + t.name + "</td><td>" + t.teamSize + "</td><td>" + `<button type="button" onclick="removedev(${t.id})">Delete</button>` + `<button type="button" onclick="showupdatedev(${t.id})">Update</button>` + "</td></tr>"
+        //console.log(t.name);
+    });
+
+    //console.log('******PUBLISHERS******')
+    document.getElementById('resultareapublishers').innerHTML = "";
+    publishers.forEach(t => {
+        document.getElementById('resultareapublishers').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>" + t.name + "</td><td>" + `<button type="button" onclick="removepub(${t.id})">Delete</button>` + `<button type="button" onclick="showupdatepub(${t.id})">Update</button>` + "</td></tr>"
+        //console.log(t.name);
+    });
 }
 
 function showupdate(id) {
     document.getElementById('gametitletoupdate').value = games.find(t => t['id'] == id)['title'];
     document.getElementById('updateformdiv').style.display = 'flex';
     gameidtoupdate = id;
+}
+
+function showupdatedev(id) {
+    document.getElementById('devnametoupdate').value = developers.find(t => t['id'] == id)['name'];
+    document.getElementById('devteamsizetoupdate').value = developers.find(t => t['id'] == id)['teamSize'];
+    document.getElementById('updateformdevdiv').style.display = 'flex';
+    devidtoupdate = id;
+}
+
+function showupdatepub(id) {
+    document.getElementById('publishernametoupdate').value = publishers.find(t => t['id'] == id)['name'];
+    document.getElementById('updateformdivpub').style.display = 'flex';
+    publisheridtoupdate = id;
 }
 
 function create() {
@@ -81,7 +191,24 @@ function create() {
             getdata();
         })
         .catch((error) => Console.error('Error: ', error));
-    
+}
+
+function createpub() {
+    let pubname = document.getElementById('publishername').value;
+    fetch('http://localhost:54503/publisher', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            { name: pubname }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => Console.error('Error: ', error));
 }
 
 function update() {
@@ -101,7 +228,45 @@ function update() {
             getdata();
         })
         .catch((error) => Console.error('Error: ', error));
+}
 
+function updatedev() {
+    document.getElementById('updateformdevdiv').style.display = 'none';
+    let devname = document.getElementById('devnametoupdate').value;
+    let teamsize = document.getElementById('devteamsizetoupdate').value;
+    fetch('http://localhost:54503/developer', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            { name: devname, id: devidtoupdate, teamSize: teamsize }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => Console.error('Error: ', error));
+}
+
+function updatepub() {
+    document.getElementById('updateformdivpub').style.display = 'none';
+    let pubname = document.getElementById('publishernametoupdate').value;
+    fetch('http://localhost:54503/publisher', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            { name: pubname, id: publisheridtoupdate }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => Console.error('Error: ', error));
 }
 
 function remove(id) {
@@ -110,6 +275,34 @@ function remove(id) {
         headers: { 'Content-Type': 'application/json', },
         body: null
         })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error) });
+}
+
+function removedev(id) {
+    fetch('http://localhost:54503/developer/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error) });
+}
+
+function removepub(id) {
+    fetch('http://localhost:54503/publisher/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
